bc1068353222c8005a1972748c8d61d13056c4c6
Franccesco Orozco <franccesco@codingdose.info>
2018-09-26 10:26:49 -0600

    add cli

--- lib/tmg.rb
+++ lib/tmg.rb
@@ -1,7 +1,41 @@
 require "tmg/version"
+require 'yaml'
+require 'httparty'
 
 module Tmg
-  def self.sayhi
-    'hi'
+  @auth = {'Authorization': self.api_key}
+
+  def self.write_credentials(username,
+                             password,
+                             outdir = "#{Dir.home}/.gem",
+                             outfile = 'credentials')
+    # Write credentials rubygems.org API access key to:
+    # /home/username/.gem/credentials
+    auth = {username: username, password: password}
+    yaml = HTTParty.get('https://rubygems.org/api/v1/api_key.yaml',
+                              basic_auth: auth)
+    File.open("#{outdir}/#{outfile}", "w") { |file| file.write(yaml) }
+  end
+
+  def self.api_key
+    credentials = YAML.load_file("#{Dir.home}/.gem/credentials")
+    credentials[:rubygems_api_key]
+  end
+
+  def self.mygems
+    auth = @auth
+    gems_list = HTTParty.get('https://rubygems.org/api/v1/gems.json',
+                             headers: auth)
+  end
+
+  def self.total_downloads()
+    total_downloads = Hash.new
+    mygems = self.mygems
+    mygems.each do |gem|
+      gem_name = gem['name']
+      gem_downloads = gem['downloads']
+      total_downloads[gem_name] = gem_downloads
+    end
+    total_downloads
   end
 end
---
+++ lib/tmg/cli.rb
@@ -0,0 +1,11 @@
+require 'tmg'
+require 'thor'
+require 'colorize'
+
+module Tmg
+  class CLI < Thor
+    def show
+      puts 'hi'
+    end
+  end
+end
